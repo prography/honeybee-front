1eb67e8dffca0e3ea7570ee98411be65029e577d
yshjft <jericoh@naver.com>
2019-04-11 20:08:47 +0900

    컴포넌트 분리

--- src/components/mainpage/Intro.vue
+++ src/components/mainpage/Intro.vue
@@ -18,17 +18,19 @@
           </div>
         </div>
       </div>
-      <div class="img_upload">
-        <i class="far fa-image img_upload_icon"></i>
-        <div for="tmp" class="img_upload_txt">이미지를 업로드해주세요</div>
-      </div>
+      <picture-input/>
     </div>
   </div>
 </template>
 
 <script>
+import pictureInput from './pictureInput.vue'
+
 export default {
   name: 'Intro',
+  components:{
+    pictureInput,
+  },
   methods:{
 
   }
@@ -66,28 +68,5 @@ export default {
   .sub_txt .highLight{
     color:yellow;
   }
-
-  .img_upload{
-    text-align: center;
-    position: relative;
-    top:12%;
-    left:26.5%;
-    width:47.2937467157%;
-    height:240px;
-    border-radius: 25px;
-    box-shadow: 0px 5px 5px #DCDCDC;
-    background-color:#f9f9f8;
-    color:#DCDCDC;
-  }
-  
-  .img_upload_icon{
-    margin-top:1%;
-    font-size:150px;
-  }
-
-  .img_upload_txt{
-    margin-bottom: 1%;
-    font-size:35px;
-  } 
 
 </style>
