d2a508d07e6977e6a908e7e78df125aca1a0d830
yshjft <jericoh@naver.com>
2019-05-28 02:27:25 +0900

    navar correction, footer height correction

--- src/components/NavBar/Navbar.vue
+++ src/components/NavBar/Navbar.vue
@@ -6,6 +6,8 @@
   </div>
   <div class="navbar-buttons">
     <navbarButton v-for="item in items" :key="item.id" :buttonText="item.name" :page="item.path"/>
+    <buttonSignIn v-if='signInOut'/>
+    <buttonSignOut v-else/>
   </div>
   <div @click="open()" class="mobile-navbar">
     <span class="bar"></span>
@@ -18,122 +20,133 @@
 
 <script>
 import navbarButton from './NavbarButton.vue'
+import buttonSignOut from './ButtonSignOut'
+import buttonSignIn from './ButtonSignIn'
 
 export default{
-  name :'navbar',
+  name :'navbarSignIn',
   components:{
-    navbarButton
-  },
-  methods:{
-    open(){
-      document.querySelector(".navbar-buttons").classList.toggle('open');
-      document.querySelector(".mobile-navbar").classList.toggle('open');
-    }
+    navbarButton,
+    buttonSignOut,
+    buttonSignIn
   },
   data(){
     const items=[
       {name:'HOME', path:'/'},
       {name:'ABOUT', path:'/about'},
-      {name:'SIGN UP', path:'/sign_up'},
-      {name:'SIGN IN', path:'/sign_in'},
     ];
     return{
-      items
+      items,
     }
-  }
+  },
+  computed:{
+    signInOut(){
+      return this.$store.getters.getSignIn; //현재 로그인 상태 가지고 온다
+    }
+  },
+  methods:{
+    open(){
+      document.querySelector(".navbar-buttons").classList.toggle('open');
+      document.querySelector(".mobile-navbar").classList.toggle('open');
+    },
+  },
+
 }
 </script>
 
 <style scoped>
-  .navbar{
+.navbar{
+  display:flex;
+  justify-content: space-between;
+  width:100%;
+  height:70px;
+  background-color: rgba(20,20,20,0.4);
+  z-index:-1;
+}
 
-    display:flex;
-    justify-content: space-between;
-    width:100%;
-    height:70px;
-    background-color: black;
-    z-index:-1;
-  }
+.navbar.notHome{
+  background-color: #111111;
+}
 
-  .navbar-logo,
-  .navbar-buttons{
-    display:flex;
-    align-items:center;
-  }
+.navbar-logo,
+.navbar-buttons{
+  display:flex;
+  align-items:center;
+}
 
-  .navbar-logo{
-    flex:0;
-  }
+.navbar-logo{
+  flex:0;
+}
 
-  .navbar-buttons{
-    flex:1;
-    justify-content:flex-end;
-  }
+.navbar-buttons{
+  flex:1;
+  justify-content:flex-end;
+}
 
-  img#logo_round{
-    width:70px;
-    height:70px;
-    margin-left:20px;
-  }
+img#logo_round{
+  width:70px;
+  height:70px;
+  margin-left:20px;
+}
 
-  #brand_title {
+#brand_title {
     color: yellow;
     font-size: 24px;
+}
+
+.mobile-navbar{
+  display:none;
+}
+
+@media(max-width:480px){
+
+  .navbar{
+    position:absolute;
   }
+
+  .navbar-buttons{
+    flex-direction: column;
+    position:fixed;
+    padding-top:100px;
+    padding-bottom:100vh;
+    width:100%;
+    background-color:#f9f9f8;
+    transform:translate(100%);
+    transition: transform 0.5s;
+  }
+
+  .navbar-buttons.open{
+    transform:translate(0);
+  }
+
   .mobile-navbar{
-    display:none;
+    display:flex;
+    flex-direction:column;
+    justify-content:space-between;
+    margin-top:15px;
+    padding:10px;
+    width:30px;
+    height:30px;
+    transition: all 0.5s;
   }
 
-  @media(max-width:480px){
-
-    .navbar{
-      position:absolute;
-    }
-
-    .navbar-buttons{
-      flex-direction: column;
-      position:fixed;
-      /* top:80px; */
-      padding-top:100px;
-      padding-bottom:100vh;
-      width:100%;
-      background-color:#f9f9f8;
-      transform:translate(100%);
-      transition: transform 0.5s;
-    }
-
-    .navbar-buttons.open{
-      transform:translate(0);
-    }
-
-    .mobile-navbar{
-      display:flex;
-      flex-direction: column;
-      justify-content:space-between;
-      margin-top:15px;
-      padding:10px;
-      width:30px;
-      height:30px;
-      transition: all 0.5s;
-    }
-
-    .mobile-navbar.open{
-      transform:rotate(45deg);
-    }
-
-    .mobile-navbar.open .bar:nth-child(1){
-      transform:rotate(90deg) translateX(40%);
-    }
-
-    .mobile-navbar.open .bar:nth-child(3){
-      opacity:0;
-    }
-
-    .bar{
-      width:100%;
-      height:2px;
-      background-color:#444020;
-      transition:all 0.5s;
-    }
+  .mobile-navbar.open{
+    transform:rotate(45deg);
   }
+
+  .mobile-navbar.open .bar:nth-child(1){
+    transform:rotate(90deg) translateX(40%);
+  }
+
+  .mobile-navbar.open .bar:nth-child(3){
+    opacity:0;
+  }
+
+  .bar{
+    width:100%;
+    height:2px;
+    background-color:#444020;
+    transition:all 0.5s;
+  }
+}
 </style>
